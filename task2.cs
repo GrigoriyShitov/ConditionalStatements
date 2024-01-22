773a5e5dc4392247723f848b43b5eb2a68e83f37
GrigoriyShitov <grisha.shitob1@gmail.com>
2024-01-22 19:12:25 +0300

    Second task from the topic

---
+++ task2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalStatement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C;
+            Console.WriteLine("Введите А:");
+            A = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Введите B:");
+            B = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Введите C:");
+            C = Convert.ToInt16(Console.ReadLine());
+            if (B == C)
+            {
+                if (A == B)
+                {
+                    Console.WriteLine("Все три точки совпадают");
+                    Console.ReadKey();
+                    return;
+                }
+                Console.WriteLine("Точки B и C совпадают и расстояние до А равно:");
+                Console.WriteLine(Math.Abs(A - B));
+                Console.ReadKey();
+                return;
+            }
+            if (Math.Abs(A - B) < Math.Abs(A - C))
+            {
+                Console.WriteLine("Точка B лежит ближе к А и расстояние до А равно:");
+                Console.WriteLine(Math.Abs(A - B));
+                Console.ReadKey();
+                return;
+            }
+            else if (Math.Abs(A - B) == Math.Abs(A - C))
+            {
+                Console.WriteLine("Точки B и C не совпадают, но равноудалены от А, расстояние до А равно:");
+                Console.WriteLine(Math.Abs(A - C));
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Точка С лежит ближе к А и расстояние до А равно");
+                Console.WriteLine(Math.Abs(A - C));
+                Console.ReadKey();
+                return;
+            }
+            Console.ReadKey();
+        }
+    }
+}
